5323303246fe94a9e45fb2d6a07941f40504f744
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-11-26 10:53:23 +1100

    revert changes to all commits today, borked.

--- _app/immutable/nodes/4.C7N6qwFu.js
+++ _app/immutable/nodes/4.C7N6qwFu.js
@@ -565,7 +565,7 @@ const Qb = () => "Log in",
     "✅ Desenhar sobre outras artes para complementar ou criar novas artes é permitido",
   n3 = (_ = {}, o = {}) => ((o.locale ?? ze()) === "en" ? t3() : r3()),
   i3 = () =>
-    "✅ All political artwork is allowed to be griefed.",
+    "✅ Griefing political party flags or portraits of politicians is allowed",
   a3 = () =>
     "✅ Desenhar sobre bandeiras de partidos e retratos de políticos é permitido",
   o3 = (_ = {}, o = {}) => ((o.locale ?? ze()) === "en" ? i3() : a3()),
